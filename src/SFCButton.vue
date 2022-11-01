9566f601f0b0eccfc9512bf02d936006f9f6da99
zone98f <1191066968@qq.com>
2022-11-01 22:44:43 +0800

    chore:init

---
+++ src/SFCButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button>SFC Button</button>
+  </template>
+  
+  <!-- <script lang="ts">
+  export default {
+    name: "SFCButton",
+  };
+  
+  </script> -->
+  <script lang="ts">
+  import { defineComponent } from "vue";
+  export default defineComponent({
+    name: "SButton",
+  });
+  </script>
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import SFCButton from "./SFCButton.vue";
+import SButton from "./button";
+
+createApp(SFCButton)
+createApp(SButton)
+.mount("#app");
